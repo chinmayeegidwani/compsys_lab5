52ba24117900e04c4e4383b176cb50da0b5c9a9b
chinmayeegidwani <chinu@LAPTOP-OA9CLONA.localdomain>
2020-11-27 13:08:10 -0500

    parallelization
    - use 8 threads
    - split cols up into sections
    - use barrier to wait for all threads at the end of generation

--- src/life.c
+++ src/life.c
@@ -67,23 +67,96 @@ game_of_life (char* outboard,
     /* HINT: in the parallel decomposition, LDA may not be equal to
        nrows! */
 
+
+	pthread_t* threads = malloc(sizeof(pthread_t) * NUM_THREADS);
+	thread_info* thread_args = malloc(sizeof(thread_info) * NUM_THREADS);
+	pthread_barrier_t* barrier_per_gen = malloc(sizeof(pthread_barrier_t));
+	pthread_barrier_init(barrier_per_gen, 0, NUM_THREADS);
+	int rows_per_thread = nrows/NUM_THREADS;
+	int row = 0;
+	int i;
+	for(i=0; i<NUM_THREADS; i++){
+		thread_args[i].ncols = ncols;
+		thread_args[i].nrows = nrows;
+		thread_args[i].inboard = inboard;
+		thread_args[i].outboard = outboard;
+		//thread_args[i].section_num = i;
+		thread_args[i].barrier_per_gen = barrier_per_gen;
+		thread_args[i].gens_max = gens_max;
+		thread_args[i].start = row;
+		row = rows_per_thread * (i+1);
+		thread_args[i].end = row;
+	} 
+
+
+	for(i=0; i<NUM_THREADS; i++){
+		thread_args[i].inboard = inboard;
+		thread_args[i].outboard = outboard;
+		pthread_create(&threads[i], NULL, process_thread, (void*) &thread_args[i]);
+	}
+
+	for(i=0; i<NUM_THREADS; i++){
+		pthread_join(threads[i], NULL);
+	}
+
+
+	free(threads);
+	free(thread_args);
+	free(barrier_per_gen);
+
+
+    /* 
+     * We return the output board, so that we know which one contains
+     * the final result (because we've been swapping boards around).
+     * Just be careful when you free() the two boards, so that you don't
+     * free the same one twice!!! 
+     */
+    return inboard;
+}
+
+/*
+
+typedef struct thread_info{
+	//int gens_max;
+	int nrows;
+	int ncols;
+	char* inboard;
+	char* outboard;
+	//int section_num;
+	pthread_barrier_t* barrier_per_gen;
+	int gens_max;
+	//int LDA;
+	int start;
+	int end;
+} thread_info;
+*/
+
+void* process_thread(void* arg){
+	thread_info* args = (thread_info*) arg;
+	const int nrows = args->nrows;
+	const int ncols = args->ncols;
+	char* inboard = args->inboard;
+	char* outboard = args->outboard;
+	pthread_barrier_t* barrier = args->barrier_per_gen;
+	int gens_max = args->gens_max;
+	int start = args->start;
+	int end = args->end; //denotes start and end columns
+
+
+	
     int curgen, i, j;
 	const int rows_per_thread = nrows/NUM_THREADS;
 	char n, s, w, e, nw, ne, sw, se, curr, count;
 	const int LDA = nrows;
 	int jwest, jeast;
-	const int nrows_1 = nrows - 1;
-  	const int nrows_2 = nrows - 2;
+
 	for(curgen=0; curgen < gens_max; curgen++){
-		for (j = 0; j < ncols; j++){
+		for (j = start; j < end; j++){
 				// LICM
 				 jwest = (j == 0)? (ncols-1):(j-1);
 				 jeast = (j == ncols-1) ? (0):(j+1);
 
-
 				// try to reuse previous cell's computed values
-
-
 
 				n = BOARD(inboard, nrows-2, j); //
 				ne = BOARD(inboard, nrows-2, jeast);//
@@ -142,73 +215,6 @@ game_of_life (char* outboard,
 			}
 			// do at the end of a generation
 		SWAP_BOARDS(outboard, inboard);
+		pthread_barrier_wait(barrier);
 	}
-
-    return inboard;
-
-
-
-
-
-
-
-
-
-
-
-/*
-	pthread_t* threads = malloc(sizeof(pthread_t) * NUM_THREADS);
-	thread_info* thread_args = malloc(sizeof(thread_info) * NUM_THREADS);
-	pthread_barrier_t* barrier_per_gen = malloc(sizeof(pthread_barrier_t));
-	pthread_barrier_init(barrier_per_gen, 0, NUM_THREADS);
-	int row = 0; 
-	for(int i=0; i<NUM_THREADS; i++){
-		thread_args[i].ncols = ncols;
-		thread_args[i].nrows = nrows;
-		thread_args[i].inboard = inboard;
-		thread_args[i].outboard = outboard;
-		//thread_args[i].section_num = i;
-		thread_args[i].barrier_per_gen = barrier_per_gen;
-		thread_args[i].gens_max = gens_max;
-		thread_args[i].start = row;
-		row = rows_per_thread * (i+1);
-		thread_args[i].end = row;
-	} 
-
-
-	for(i=0; i<NUM_THREADS; i++){
-		thread_args[i].inboard = inboard;
-		thread_args[i].outboard = outboard;
-		pthread_create(&threads[i], NULL, process_thread, (void*) &thread_args[i]);
-	}
-
-	for(i=0; i<NUM_THREADS; i++){
-		pthread_join(threads[i], NULL);
-	}
-
-
-	free(threads);
-	free(thread_args);
-	free(barrier_per_gen);
-*/
-
-    /* 
-     * We return the output board, so that we know which one contains
-     * the final result (because we've been swapping boards around).
-     * Just be careful when you free() the two boards, so that you don't
-     * free the same one twice!!! 
-     */
-
 }
-
-/*
-
-typedef struct thread_info{
-	int nrows;
-	int ncols;
-	char* inboard;
-	char* outboard;
-	int section_num;
-} thread_info;
-*/
-
